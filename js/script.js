bac113796daf5c5be7da69f05bf40886248c0756
busraozata <busraozata1998@gmail.com>
2022-11-24 17:05:49 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,103 @@
+const menuToggle = document.querySelector("#toggle-btn");
+const navigation = document.querySelector(".navigation");
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+
+  if (menuToggle.innerHTML === `<i class="fa-solid fa-xmark"></i>`) {
+    menuToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    menuToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+};
+
+$("ul li").on("click", function () {
+  $(this).toggleClass("show");
+  $(this).siblings().removeClass("show");
+});
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
-    dynamicBullets: true,
     clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".mySwiperDoctor", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  initialSlide: 2,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 70,
+    },
+    1300: {
+      slidesPerView: 3,
+      spaceBetween: 70,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiperGallery", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  initialSlide: 2,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 70,
+    },
+    1300: {
+      slidesPerView: 3,
+      spaceBetween: 70,
+    },
   },
 });
 
 $(".tab-menu a").hover(function (e) {
   e.preventDefault();
   $(this).tab("show");
+  /*  $(this).addClass("active"); */
 });
+
+$(".tab-menu li")
+  .mouseover(function () {
+    $(this).addClass("active");
+  })
+  .mouseout(function () {
+    $(this).removeClass("active");
+  });
